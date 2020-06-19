513af668857463d99a370e27a95a27747db6ebc7
LuckyLiar <40919210+LuckyLiar@users.noreply.github.com>
2020-06-19 18:47:07 +0500

    Revert "Убран лишний лог"
    This reverts commit 83f50b51d9262998fface8092f625c9e26b3faeb.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -63,6 +63,7 @@ router.post(
       }
 
       const isGood = await bcrypt.compare(password, user.password);
+      console.log(isGood);
       if (!isGood) {
         return res.status(400).json({ message: "Bad password" });
       }
