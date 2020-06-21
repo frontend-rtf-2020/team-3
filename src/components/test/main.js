857dfe07bada945f33c469224e0f1724a22f2f1a
Sokolov Max <maxerdan@yandex.ru>
2020-06-21 23:54:54 +0500

    Revert "clean master branch"
    This reverts commit c1817eeb2b6c61767fc1b747f8a2f0067780393b.

---
+++ src/components/test/main.js
@@ -0,0 +1,20 @@
+/* 
+import React, { Component } from 'react';
+
+import {Switch, Route} from 'react-router-dom';
+import {Registrate} from '../Forms/Registrate';
+import {LogIn} from './Login';
+
+export class Main extends Component{
+
+    render(){
+        return (
+
+            <h1>Добро Пожаловать!</h1>
+            <Switch>
+                <Route path="/login" component={LogIn}/>
+                <Route path="/registrate" component={Reg}/>
+            </Switch>
+
+    }
+} */
